e2b8305a93274994488c27eecb5ff6272a93f74b
MariusRa <marius.raudys@stud.kitm.lt>
2021-10-28 18:53:53 +0300

    first cimmit

---
+++ my-app/src/components/app/app.jsx
@@ -0,0 +1,14 @@
+import Header from '../../components/header/header';
+import Main from '../../components/main/main';
+import Footer from '../../components/footer/footer';
+
+function App() {
+    return (
+        <div className='reactDemo'>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </div>
+    );
+}
+export default App;
---
+++ my-app/src/components/main/main.jsx
@@ -0,0 +1,10 @@
+import './main.scss';
+
+function Main() {
+    return (
+        <main>
+            <p>Testas</p>
+        </main>
+    );
+}
+export default Main;
